e3449c9414e34b31cec777884939a5b78a248f72
maurimooreno <maurimooreno@gmail.com>
2022-04-11 19:57:25 -0300

    relaciones entre tablas terminadas

---
+++ db/models/historialOcupante.model.js
@@ -0,0 +1,33 @@
+const {Model, DataTypes, Sequelize} = require('sequelize')
+
+const HISTORIAL_OCUPANTE_TABLE = 'habitaciones';
+
+const HistorialOcupanteSchema = {
+  
+    checkIn:{
+        type: DataTypes.DATEONLY,
+        allowed: false
+    },
+    checkOut:{
+        type: DataTypes.DATEONLY,
+        allowed: false
+    }
+}
+
+class HistorialOcupante extends Model {
+  static associate() {
+    // asociaciones
+  }
+
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: HISTORIAL_OCUPANTE_TABLE,
+      modelName: 'Historia_Ocupante',
+      timestamps: false
+    }
+  }
+}
+
+
+module.exports = { HISTORIAL_OCUPANTE_TABLE, HistorialOcupanteSchema, HistorialOcupante }
