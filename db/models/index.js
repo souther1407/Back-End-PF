485d7dd93ae18a1f2822574eb899a416a7061813
maurimooreno <maurimooreno@gmail.com>
2022-04-11 19:04:27 -0300

    definiendo los modelos

--- db/models/index.js
+++ db/models/index.js
@@ -2,14 +2,14 @@ const { Usuario, UsuarioSchema} = require('./usuario.model');
 const { Habitacion, HabitacionSchema} = require('./habitacion.model');
 const { ReservaCama, ReservaCamaSchema} = require('./reservaCama.model');
 const { Cama, CamaSchema} = require('./cama.model')
-const { Rol, RolSchema} = require('./rol.model');
+const { Huesped, HuespedSchema } = require('./huesped.model');
 
 function setupModels(sequelize) {
+  Huesped.init(HuespedSchema, Huesped.config(sequelize))
   Usuario.init(UsuarioSchema, Usuario.config(sequelize));
   Habitacion.init(HabitacionSchema, Habitacion.config(sequelize));
   ReservaCama.init(ReservaCamaSchema, ReservaCama.config(sequelize));
   Cama.init(CamaSchema, Cama.config(sequelize));
-  Rol.init(RolSchema, Rol.config(sequelize));
 }
 
 module.exports = setupModels;
