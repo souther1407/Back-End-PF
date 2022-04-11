db2608f33a51058606d2edcb6079febaa04cdf23
Eric Casanova <kilbane1407@gmail.com>
2022-04-11 18:58:13 -0300

    tabla tipoDoc y nacionalidades agregadas

--- db/models/index.js
+++ db/models/index.js
@@ -3,6 +3,9 @@ const { Habitacion, HabitacionSchema} = require('./habitacion.model');
 const { ReservaCama, ReservaCamaSchema} = require('./reservaCama.model');
 const { Cama, CamaSchema} = require('./cama.model')
 const { Rol, RolSchema} = require('./rol.model');
+const {Nacionalidades,nacionalidadesSchema} = require("./nacionalidad.model")
+
+const {TipoDocumento,tipoDocumentoSchema} = require("./tipoDocumento.model")
 
 function setupModels(sequelize) {
   Usuario.init(UsuarioSchema, Usuario.config(sequelize));
@@ -10,6 +13,8 @@ function setupModels(sequelize) {
   ReservaCama.init(ReservaCamaSchema, ReservaCama.config(sequelize));
   Cama.init(CamaSchema, Cama.config(sequelize));
   Rol.init(RolSchema, Rol.config(sequelize));
+  Nacionalidades.init(nacionalidadesSchema,Nacionalidades.config(sequelize));
+  TipoDocumento.init(tipoDocumentoSchema,TipoDocumento.config(sequelize));
 }
 
 module.exports = setupModels;
---
+++ db/models/tipoDocumento.model.js
@@ -0,0 +1,34 @@
+const {Model, DataTypes, Sequelize} = require('sequelize')
+
+const TIPO_DOCUMENTO_TABLE = 'tipo_documento';
+
+const tipoDocumentoSchema = {
+  id: {
+    allownull: false,
+    primaryKey: true,
+    type: DataTypes.UUID,     
+    defaultValue: DataTypes.UUIDV4,
+  },
+  nombre: {
+    allowNull: false,
+    type: DataTypes.STRING
+  },
+}
+
+class TipoDocumento extends Model {
+  static associate() {
+    // asociaciones
+  }
+
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: TIPO_DOCUMENTO,
+      modelName: 'TipoDocumento',
+      timestamps: false
+    }
+  }
+}
+
+
+module.exports = { TIPO_DOCUMENTO_TABLE, tipoDocumentoSchema, TipoDocumento }
