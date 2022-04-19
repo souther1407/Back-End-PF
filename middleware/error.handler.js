304e011a83a5b018ce90bfa3dc233f74b056f936
Eric Casanova <kilbane1407@gmail.com>
2022-04-19 14:22:36 -0300

    fixed

--- middleware/error.handler.js
+++ middleware/error.handler.js
@@ -1,4 +1,4 @@
-/* const boom = require('@hapi/boom')
+const boom = require('@hapi/boom')
 
 function logErrors (err, req, res, next) {
   console.error(err);
@@ -6,38 +6,24 @@ function logErrors (err, req, res, next) {
 }
 
 function errorHandler (err, req, res, next) {
-  if(err){
-    res.status(500).json({
-      message: err.message,
-      stack: err.stack,
-    })
-  }else{
-    next(err)
-  }
+  next()
   
 }
 
 function boomErrorHandler (err, req, res, next) {
-  if (err.isBoom) {
-    const { output } = err;
-    res.status(output.statusCode).json(output.payload)
-  }
+  
 next(err);
 }
 
 function error404Handler (req, res, next) {
-  res.status(404)
-  res.send ({
-  message: boom.notFound('el recurso que busca no existe')
-  })
+  
   next()
 }
- */
-/* 
+
+
 module.exports= {
   logErrors,
   errorHandler,
   boomErrorHandler,
   error404Handler
 }
- */
