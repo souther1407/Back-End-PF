527872a9a6be32b8912031de8c6e0fda98bb4e19
Eric Casanova <kilbane1407@gmail.com>
2022-04-19 20:35:21 -0300

    deploy

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -5,6 +5,12 @@ const { Usuario } = require('../db/models/usuario.model');
 const { checkGoogleToken } = require('../middleware/auth.handler.js');
 const AuthService = require('../services/auth.services');
 const service = new AuthService;
+
+
+
+//TODO: usar lo de rodrigo
+const bcrypt = require('bcrypt');
+
 const router = express.Router();
 
 require("../utils/auth/strategies/Google.strategy")
@@ -51,10 +57,11 @@ router.get("/prohibido",passport.authorize("google"), (req, res) =>{
 router.post("/auth/google", async (req, res) => {
   const { googleId } = req.body;
   console.log(googleId)
+  console.log
   const token = req.headers.authorization.split(" ")[1]
-  console.log(req.headers.authorization)
+  const payload = jwt.decode(token)
   try {
-    res.json({ success: true, googleId, token})
+    res.json({ success: true, googleId, payload})
   } catch (error) {
     res.json(error)
 }
@@ -63,8 +70,35 @@ router.post("/auth/google", async (req, res) => {
   //TODO: busco el googleId en la base, si no está, registro el usuario
 
 
-/* router.post("/signup",(req ,res) => {
-  const {} =
+router.post("/signup",async (req ,res) => {
+
+  const {dni,googleId,nombre,apellido,email,password} = req.body;
+  let existeUsuarioGoogle;
+  let existeUsuario;
+
+  try {
+    
+    if(googleId){
+       existeUsuarioGoogle = await Usuario.findOne({where:{ googleId }});
+    }else{
+       existeUsuario = await Usuario.findOne({where:{dni}});
+    }
+  
+    if(existeUsuarioGoogle !== null || existeUsuario !== null){
+
+      return res.json({success: false, msg:"Ya existe el usuario"})
+
+    }else{
+
+      const newUser = await Usuario.create({dni,googleId,nombre,apellido,email,password:await bcrypt.hash(password+email,12)})
+      res.json({success:true, msg:"Usuario creado con éxito"})
+
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(400).json({error})
+  }
+
 })
- */
+
 module.exports = router;
--- utils/auth/strategies/Google.strategy.js
+++ utils/auth/strategies/Google.strategy.js
@@ -1,6 +1,7 @@
 const passport = require("passport")
 const GoogleStrategy = require("passport-google-oauth20").Strategy
 const jwt = require("jsonwebtoken")
+
 const GOOGLE_CLIENT_ID = "540051645175-sbuak0uu5auodj9ipes8lklhgeg39kfo.apps.googleusercontent.com"
 const GOOGLE_SECRET_ID = "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"
 
@@ -12,7 +13,6 @@ passport.use(new GoogleStrategy({
 },
 ( accessToken, refreshToken, profile, done ) => {
 try {
-    
     const asd = jwt.verify(accessToken, GOOGLE_SECRET_ID)
     console.log(asd)
     done(null,profile)
