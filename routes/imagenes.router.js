5b301d4a7ea97d45e8a82e5d9b47525d32e5a21c
Aragornz <70964354+aragornz325@users.noreply.github.com>
2022-04-22 08:37:06 -0300

    :construction: segurizacion de rutas

---
+++ routes/imagenes.router.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const ImagenService = require('../services/imagenes.services');
+const validatorHandler = require('../middleware/validator.handler');
+const {chequearRoles, } = require('../middleware/auth.handler')
+const { updateUserSchema, createUserSchema, getUserSchema } = require('../schemas/usuario.schema');
+const passport = require('passport'); 
+const router = express.Router();
+const service = new ImagenService
+
+router.get('/',
+passport.authenticate('jwt', {session: false}),
+chequearRoles("administrador", "recepcionisa"),
+async (req, res, next) => {
+  try {
+    const imagenes = service.mostrarTodo()
+    res.status(200).json(imagenes)  
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/:id',
+passport.authenticate('jwt', {session: false}),
+chequearRoles("administrador", "recepcionisa"),
+//validatorHandler(getUserSchema, 'params'),
+async (req, res, next) => {
+  try {
+    const { id } = req.query 
+    const imagen = service.buscarPorId(id)
+    res.status(200).json(imagen)  
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post('/',
+ passport.authenticate('jwt', {session: false}),
+ chequearRoles("administrador", "recepcionista", "cliente"),
+ //validatorHandler(createUserSchema, 'body'), 
+  async (req, res, next) => {
+    try {
+      const {id, imagen} = req.body
+      const nuevaImagen = service.crear(id, imagen)
+      res.status(201).json(nuevaImagen);
+    } catch (error) {
+      next(error)
+    }
+  }
+);
+
+router.patch('/:id',
+  passport.authenticate('jwt', {session: false}),
+  chequearRoles("administrador", "recepcionista", "cliente"),
+ // validatorHandler(getUserSchema, 'params'),
+ // validatorHandler(updateUserSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.delete('/:id',
+  passport.authenticate('jwt', {session: false}),
+  chequearRoles("administrador", "recepcionista", "cliente"),
+  //validatorHandler(getUserSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+module.exports = router;
