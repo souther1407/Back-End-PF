65ce109d104ae2db0e61b1be882fecd1b72f158a
Aragornz <70964354+aragornz325@users.noreply.github.com>
2022-04-22 22:52:01 -0300

    :construction: segurizando rutas

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -104,7 +104,7 @@ Habitacion.hasMany(Imagen, {onDelete: 'cascade'});
 Imagen.belongsTo(Habitacion)
 
 
-sequelize.sync({ force: false })
+sequelize.sync({ force: true })
   .then(() => {
     console.log(`base de datos creada/actualizada `);
 
