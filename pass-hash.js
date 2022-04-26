a964e741f15f3368acfe54631c0457c179006f02
Aragornz <70964354+aragornz325@users.noreply.github.com>
2022-04-26 16:16:25 -0300

    sacando mensajes

--- pass-hash.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcrypt');
-
-async function hasheadora(){
-const myPassword = 'rodrigoadmin .123'
-const hash = await bcrypt.hash(myPassword, 12);
-console.log(hash)
-}
-
-hasheadora()
