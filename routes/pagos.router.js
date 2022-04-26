041f7a3cf642d0b28edefa8814b2c39b3925fec6
Eric Casanova <kilbane1407@gmail.com>
2022-04-26 19:41:24 -0300

    pull

--- routes/pagos.router.js
+++ routes/pagos.router.js
@@ -5,8 +5,10 @@ const service = new pagoService()
 
 router.post('/checkout', async (req, res) => {
     try {
-        console.log(req.body)
-        const payment = await service.crearPago(req.body)
+
+        const {cart} = req.body
+        console.log(cart)
+        const payment = await service.crearPago(cart)
         return res.status(200).json(payment);
     } catch (error) {
         console.log(error);
