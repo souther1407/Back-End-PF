4abacfd75097f161a85a6275e88e73ea5c44748a
Eric Casanova <kilbane1407@gmail.com>
2022-04-27 12:11:16 -0300

    pull

--- routes/pagos.router.js
+++ routes/pagos.router.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const passport = require('passport')
+const { checkApiKey } = require('../middleware/auth.handler')
 const router = express.Router()
 const pagoService = require('../services/pagos.services')
 const service = new pagoService()
 
-router.post('/checkout', async (req, res) => {
+router.post('/checkout',checkApiKey,passport.authenticate('jwt', {session: false}), async (req, res) => {
     try {
 
         const {cart} = req.body
